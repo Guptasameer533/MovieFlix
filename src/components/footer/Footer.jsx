93a0c1d0044938f93cf9900e07a275c9dc89529d
guptasameer533 <guptasameer533@gmail.com>
2024-05-30 18:16:07 +0530

    initial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+    FaGithub,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                    MovieFlix is a comprehensive online platform dedicated to 
+                    helping movie enthusiasts discover and explore the 
+                    vast world of cinema. Our mission is to provide a 
+                    seamless and engaging experience for users to search, 
+                    browse, and find information about their favorite movies, 
+                    as well as uncover new and exciting titles.
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaGithub />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
